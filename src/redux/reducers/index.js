1b1e167cd5b45e3a87ecde05eda598fb3b3021a1
Edoardo <edfl.tunzi@gmail.com>
2024-12-13 10:22:29 +0100

    repository initialised, vite,redux,bootstrap installed and linked

---
+++ src/redux/reducers/index.js
@@ -0,0 +1,15 @@
+const initialState = {
+  value: 0
+  /*cart: {
+        content: []
+        } esempio lezione*/
+};
+
+const mainReducer = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default mainReducer;
